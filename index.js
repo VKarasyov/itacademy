1a7118b9e13ed65af37d893f9bea8ef5831e3ad7
VKarasyov <vlad.karasyov.00@mail.ru>
2023-05-05 22:53:48 +0300

    п

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-let getNum = document.getElementById("Number").value;
-getNum = getNum.split(" ");
-
 function result(){
+    let getNum = document.getElementById("Number").value;
+    getNum = getNum.split(" ");
     let sum = 0;
     for (let i = 0; i < getNum.length; i++) {
         sum += Number( getNum[i] ); 
